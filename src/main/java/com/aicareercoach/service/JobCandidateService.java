fb2ae4b0d9a18d9bd0afa5d80ef015f80a1f20e5
MohamedSohil85 <mimo_mnb@hotmail.com>
2025-07-02 21:45:10 +0200

    add new code implementation

--- src/main/java/com/aicareercoach/service/JobCandidateService.java
+++ src/main/java/com/aicareercoach/service/JobCandidateService.java
@@ -1,8 +1,14 @@
 package com.aicareercoach.service;
 
+import com.aicareercoach.persistence.JobCandidateRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JobCandidateService {
 
+private final JobCandidateRepository jobCandidateRepository;
+
+    public JobCandidateService(JobCandidateRepository jobCandidateRepository) {
+        this.jobCandidateRepository = jobCandidateRepository;
+    }
 }
